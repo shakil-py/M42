f0ad67839a33ac85ec116f3f527b9b3d963eedee
Shakil Haque <shakilhaquepy@gmail.com>
2026-01-11 11:36:57 +0600

    project set up done

---
+++ kn-caf/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-center text-3xl font-bold '>hi this project setup</h1>
+    </>
+  )
+}
+
+export default App
